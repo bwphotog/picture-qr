649401b6b24af1bbb61e33bb837e230aad885e1a
bwphotog <panlobpanlob@gmail.com>
2025-05-23 23:51:16 +0700

    Create pgsql.sql

---
+++ pgsql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE albums (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  folder_id TEXT,
+  cover_photo_id TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  album_id INTEGER REFERENCES albums(id) ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  file_id TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
