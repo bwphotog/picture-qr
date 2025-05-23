f04eb803e75b4100dcc97a08cce8dec592dd6dd4
bwphotog <panlobpanlob@gmail.com>
2025-05-23 17:34:18 +0700

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
@@ -9,7 +9,6 @@ app.get('/', (req, res) => {
   res.send('Picture QR Backend is running!');
 });
 
-// ✅ ต้องมีบรรทัดนี้เพื่อ bind พอร์ต
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
