007cb439804c935ed3ebf6945c321799c72c9fde
bwphotog <panlobpanlob@gmail.com>
2025-05-23 23:38:07 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-// frontend/src/App.jsx
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AdminLogin from './pages/AdminLogin'
 import AdminAlbums from './pages/AdminAlbums'
 import AdminAlbumPhotos from './pages/AdminAlbumPhotos'
-// ... import หน้าอื่นๆ
 
 function App() {
   return (
@@ -13,7 +10,6 @@ function App() {
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminAlbums />} />
         <Route path="/admin/albums/:id" element={<AdminAlbumPhotos />} />
-        {/* route หน้าอื่น ๆ */}
       </Routes>
     </Router>
   )
