c1746c14c736aa3ea181179ef14134b3574f58fb
bwphotog <panlobpanlob@gmail.com>
2025-05-23 23:42:46 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AdminLogin from './pages/AdminLogin'
 import AdminAlbums from './pages/AdminAlbums'
 import AdminAlbumPhotos from './pages/AdminAlbumPhotos'
+import UploadPhoto from './pages/UploadPhoto'
+import SetCoverPhoto from './pages/SetCoverPhoto'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminAlbums />} />
         <Route path="/admin/albums/:id" element={<AdminAlbumPhotos />} />
+        <Route path="/admin/albums/:id/upload" element={<UploadPhoto />} />
+        <Route path="/admin/albums/:id/set-cover" element={<SetCoverPhoto />} />
       </Routes>
     </Router>
   )
